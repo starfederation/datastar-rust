06cda3768ca0ab646287da05e2d6bc81fe009864
Ryan Eno <ryan.eno@elastic.co>
2025-07-27 21:02:07 -0700

    Remove state, use signals

--- examples/axum-activity-feed.rs
+++ examples/axum-activity-feed.rs
@@ -12,23 +12,8 @@ use {
         prelude::{ElementPatchMode, PatchElements, PatchSignals},
     },
     serde::{Deserialize, Serialize},
-    std::sync::Arc,
-    tokio::sync::RwLock,
     tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt},
 };
-
-pub struct State {
-    pub feed: Vec<String>,
-    pub count: Count,
-}
-
-pub struct Count {
-    pub all: u32,
-    pub done: u32,
-    pub warn: u32,
-    pub fail: u32,
-    pub info: u32,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -41,33 +26,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let state = Arc::new(RwLock::new(State {
-        feed: Vec::new(),
-        count: Count {
-            all: 0,
-            done: 0,
-            warn: 0,
-            fail: 0,
-            info: 0,
-        },
-    }));
-
-    let state_generate = state.clone();
-    let state_info = state.clone();
-    let state_done = state.clone();
-    let state_warn = state.clone();
-    let state_fail = state.clone();
-
-    let event = { move |level, state| async move { event(level, state).await } };
-    let generate = { move |signals| async move { generate(signals, state_generate).await } };
-
     let app = Router::new()
         .route("/", get(index))
         .route("/event/generate", post(generate))
-        .route("/event/info", post(move || event(Status::Info, state_info)))
-        .route("/event/done", post(move || event(Status::Done, state_done)))
-        .route("/event/warn", post(move || event(Status::Warn, state_warn)))
-        .route("/event/fail", post(move || event(Status::Fail, state_fail)));
+        .route(
+            "/event/info",
+            post(move |signals| event(Status::Info, signals)),
+        )
+        .route(
+            "/event/done",
+            post(move |signals| event(Status::Done, signals)),
+        )
+        .route(
+            "/event/warn",
+            post(move |signals| event(Status::Warn, signals)),
+        )
+        .route(
+            "/event/fail",
+            post(move |signals| event(Status::Fail, signals)),
+        );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -89,6 +66,11 @@ pub struct Signals {
     pub interval: u64,
     pub events: u64,
     pub generating: bool,
+    pub total: u64,
+    pub done: u64,
+    pub warn: u64,
+    pub fail: u64,
+    pub info: u64,
 }
 
 #[derive(Clone, PartialEq, Eq, Deserialize)]
@@ -99,38 +81,35 @@ pub enum Status {
     Fail,
 }
 
-async fn generate(
-    ReadSignals(signals): ReadSignals<Signals>,
-    state: Arc<RwLock<State>>,
-) -> impl IntoResponse {
+async fn generate(ReadSignals(signals): ReadSignals<Signals>) -> impl IntoResponse {
     Sse::new(stream! {
-        let elements = r#"<div id="feed"></div>"#.to_string();
-        let patch = PatchElements::new(elements);
-        let sse_event = patch.write_as_axum_sse_event();
-        yield Ok::<_, Infallible>(sse_event);
-
-        let signals_generating = serde_json::to_string(&Signals{
-            generating: true,
-            interval: signals.interval,
-            events: signals.events,
-        }).unwrap();
-        let patch = PatchSignals::new(signals_generating);
-        let sse_event = patch.write_as_axum_sse_event();
-        yield Ok::<_, Infallible>(sse_event);
-
+        let mut total = signals.total;
+        let mut done = signals.done;
         for _ in 1..=signals.events {
-            let mut state = state.write().await;
-            let elements = event_entry(&mut state, &Status::Done, "Auto");
+            total += 1;
+            done += 1;
+            let elements = event_entry(total, &Status::Done, "Auto");
             let patch = PatchElements::new(elements).selector("#feed").mode(ElementPatchMode::After);
             let sse_event = patch.write_as_axum_sse_event();
+            yield Ok::<_, Infallible>(sse_event);
+            let signals_generating = serde_json::to_string(&Signals{
+                generating: true,
+                total,
+                done,
+                ..signals
+            }).unwrap();
+            let patch = PatchSignals::new(signals_generating);
+            let sse_event = patch.write_as_axum_sse_event();
+
             yield Ok::<_, Infallible>(sse_event);
             tokio::time::sleep(Duration::from_millis(signals.interval)).await;
         }
 
         let signals_done = serde_json::to_string(&Signals{
             generating: false,
-            interval: signals.interval,
-            events: signals.events,
+            total,
+            done,
+            ..signals
         }).unwrap();
         let patch = PatchSignals::new(signals_done);
         let sse_event = patch.write_as_axum_sse_event();
@@ -138,49 +117,65 @@ async fn generate(
     })
 }
 
-async fn event(status: Status, state: Arc<RwLock<State>>) -> impl IntoResponse {
+async fn event(status: Status, ReadSignals(signals): ReadSignals<Signals>) -> impl IntoResponse {
     Sse::new(stream! {
-        let mut state = state.write().await;
-        let elements = event_entry(&mut state, &status, "Manual");
+        let total = signals.total + 1;
+        let mut done = signals.done;
+        let mut warn = signals.warn;
+        let mut fail = signals.fail;
+        let mut info = signals.info;
+        match status {
+            Status::Done => done += 1,
+            Status::Warn => warn += 1,
+            Status::Fail => fail += 1,
+            Status::Info => info += 1,
+        }
+        let elements = event_entry(total, &status, "Manual");
         let patch = PatchElements::new(elements).selector("#feed").mode(ElementPatchMode::After);
         let sse_event = patch.write_as_axum_sse_event();
-
         yield Ok::<_, Infallible>(sse_event);
+
+        let signals = serde_json::to_string(&Signals {
+            total,
+            done,
+            warn,
+            fail,
+            info,
+            ..signals
+        }).unwrap();
+        let patch = PatchSignals::new(signals);
+        let sse_signal = patch.write_as_axum_sse_event();
+        yield Ok::<_, Infallible>(sse_signal);
     })
 }
 
-fn event_entry(state: &mut State, status: &Status, prefix: &str) -> String {
-    state.count.all += 1;
+fn event_entry(index: u64, status: &Status, prefix: &str) -> String {
     let timestamp = chrono::Utc::now()
         .format("%Y-%m-%d %H:%M:%S%.3f")
         .to_string();
     match status {
         Status::Done => {
-            state.count.done += 1;
             format!(
                 "<div id='event-{}' class='text-green-500'>{} [ ✅ Done ] {} event {}</div>",
-                state.count.all, timestamp, prefix, state.count.all
+                index, timestamp, prefix, index
             )
         }
         Status::Warn => {
-            state.count.warn += 1;
             format!(
                 "<div id='event-{}' class='text-yellow-500'>{} [ ⚠️ Warn ] {} event {}</div>",
-                state.count.all, timestamp, prefix, state.count.all
+                index, timestamp, prefix, index
             )
         }
         Status::Fail => {
-            state.count.fail += 1;
             format!(
                 "<div id='event-{}' class='text-red-500'>{} [ ❌ Fail ] {} event {}</div>",
-                state.count.all, timestamp, prefix, state.count.all
+                index, timestamp, prefix, index
             )
         }
         Status::Info => {
-            state.count.info += 1;
             format!(
                 "<div id='event-{}' class='text-blue-500'>{} [ ℹ️ Info ] {} event {}</div>",
-                state.count.all, timestamp, prefix, state.count.all
+                index, timestamp, prefix, index
             )
         }
     }
